974a8d2170f357935e9545c632674c7fd326f761
luca-padula <lucamp9843@gmail.com>
2019-09-22 13:06:07 -0400

    - updated insert query in user-service.js registerUser() function

--- user-service.js
+++ user-service.js
@@ -23,23 +23,27 @@ module.exports.registerUser = function(userData) {
         }
         else {
             bcrypt.hash(userData.password, 10).then(function(hash) {
-                userData.password = hash;
-                console.log(hash);
-                db.query('INSERT INTO `Users` (userName, email, password) VALUES ?',
-                [userdata.userName, userData.email, userData.password], function(err, results) {
+                let newUser = {
+                    "userName": userData.userName,
+                    "email": userData.email,
+                    "password": hash
+                };
+                console.log(newUser.password);
+                let query = db.query('INSERT INTO `Users` (userName, email, password) VALUES (?)', newUser, function(err, results) {
                     if (err) {
-                        console.log('error');
-                        if (err.code == 1062) {
+                        console.log(err);
+                        if (err.errno == 1062) {
                             reject('Username already taken');
                         }
                         else {
-                            reject('Error creating user: ' + err);
+                            reject('Error creating user');
                         }
                     }
                     else {
-                        resolve('User ' + userData.userName + ' successfully registered');
+                        resolve('User ' + newUser.userName + ' successfully registered');
                     }
                 });
+                console.log(query);
             })
             .catch(function(err) {
                 reject(err);
